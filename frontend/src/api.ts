a379b67acd1b1bfff32cf36a6a3a8bdbee492bed
Felipe180608 <felipebordignon44@gmail.com>
2026-01-08 13:03:33 -0300

    chore: adjust environment settings for local setup

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/src/routes",
+    baseURL: "http://localhost/backend/src/routes",
     withCredentials: true
 });
 
